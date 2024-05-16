620aebd2ac8337c71f6d3e28cbb96dc2b646a8a2
egorkaw2w <85741587+egorkaw2w@users.noreply.github.com>
2024-05-16 09:57:29 +0300

    bububu

---
+++ BiblPrakt/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using jsoner;
+
+namespace BiblPrakt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Serializer_Click(object sender, RoutedEventArgs e)
+        {
+            SerialDes.Ser(NameBar.Text,AnimBar.Text);
+            NameBar.Text = "";
+            AnimBar.Text = "";
+        }
+
+        private void Deserialiser_Click(object sender, RoutedEventArgs e)
+        {
+            List<human> kek = SerialDes.Des<List<jsoner.human>>("chel.json");
+            NameBar.Text = kek[0].name;
+            AnimBar.Text = kek[0].favanim;
+            
+        }
+    }
+}
---
+++ jsoner/SerialDes.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+namespace jsoner
+{
+    public class SerialDes
+    {
+        List<human> humanList;
+        public static void Ser(string name, string favan)
+        {
+            List<human> humanList = new List<human>();
+            human asd = new human(name, favan);
+            humanList.Add(asd);
+            string json = JsonConvert.SerializeObject(humanList);
+            File.WriteAllText("chel.json", json);
+        }
+        public static T Des<T>(string filename)
+        {
+            T types = JsonConvert.DeserializeObject<T>(File.ReadAllText(filename));
+            return types;
+        }
+
+    }
+}
+
